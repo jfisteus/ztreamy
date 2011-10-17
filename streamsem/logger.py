5a24200997a68d50857da468025a42156accc338
Jesus Arias Fisteus <jaf@it.uc3m.es>
2011-10-17 16:08:07 +0200

    A logger interface for producing evaluation traces.
    It should have been added some commits ago, but I forgot to. Several previous
    commits depend on this commit.

---
+++ streamsem/logger.py
@@ -0,0 +1,48 @@
+import time
+
+class StreamsemLogger(object):
+    def __init__(self, node_id, filename):
+        self.log_file = open(filename, 'a')
+        self.log_file.write('# Node: %s\n#\n'%node_id)
+
+    def close(self):
+        self.log_file.close()
+
+    def flush(self):
+        self.log_file.flush()
+
+    def event_published(self, event):
+        parts = ['event_publish', event.event_id, timestamp()]
+        self.log_file.write('\t'.join(parts))
+        self.log_file.write('\n')
+
+    def event_dispatched(self, event):
+        parts = ['event_dispatch', event.event_id, timestamp()]
+        self.log_file.write('\t'.join(parts))
+        self.log_file.write('\n')
+
+    def event_delivered(self, event):
+        parts = ['event_deliver', event.event_id, timestamp()]
+        self.log_file.write('\t'.join(parts))
+        self.log_file.write('\n')
+
+
+class StreamsemLoggerStub(object):
+    def close(self):
+        pass
+
+    def event_created(self, event):
+        pass
+
+    def event_dispatched(self, event):
+        pass
+
+    def event_delivered(self, event):
+        pass
+
+
+def timestamp():
+    return '%.6f'%time.time()
+
+# Default logger
+logger = StreamsemLoggerStub()
