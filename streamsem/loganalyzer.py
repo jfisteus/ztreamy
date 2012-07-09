401c858d8e933a8fcf83756662844e6fa23b68b9
Jesus Arias Fisteus <jaf@it.uc3m.es>
2012-07-09 16:52:26 +0200

    Added a GPLv3 licence to the software.

--- streamsem/loganalyzer.py
+++ streamsem/loganalyzer.py
@@ -1,4 +1,21 @@
-""" Analyzes log files to obtain performance statistics.
+# streamsem: a framework for publishing semantic events on the Web
+# Copyright (C) 2011-2012 Jesus Arias Fisteus
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see
+# <http://www.gnu.org/
+#
+"""Analyzes log files to obtain performance statistics.
 
 """
 import sys
