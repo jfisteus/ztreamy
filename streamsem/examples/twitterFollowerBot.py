9dc25d60f950f4ea3dd71c1c42059319ff5b092e
Jesus Arias Fisteus <jaf@it.uc3m.es>
2012-07-09 11:13:08 +0200

    Code cleanup at the twitter examples.

--- streamsem/examples/twitterFollowerBot.py
+++ streamsem/examples/twitterFollowerBot.py
@@ -1,14 +1,11 @@
 import tornado.ioloop
 import tornado.options
-import logging
 
 from rdflib import Graph
 from rdflib import Namespace
 from rdflib import Literal
 from rdflib import URIRef
 
-import streamsem
-from streamsem import events
 from streamsem import rdfevents
 from streamsem import client
 from streamsem import logger
@@ -20,62 +17,50 @@ from twitterRESTclient import TwitterRESTclient
 
 class TwitterFollowerBot():
 
-    def __init__(self, publisher, user, time = 10, app_id = "TwitterFollowerBot", source_id = "TwitterFollower0"):
-
-	self.NS = Namespace("http://webtlab.it.uc3m.es/")
-	self.DC = Namespace("http://purl.org/dc/elements/1.1/")
-	self.publisher = publisher
-	self.app_id = app_id
-	self.source_id = source_id
-	self.io_loop = tornado.ioloop.IOLoop.instance()
-	self.num_events_created = 0
-	self.scheduler = utils.get_scheduler("const[" + str(time) + "]")
-	self.client = TwitterRESTclient(user)
-	self.last_id = 0
-
+    def __init__(self, publisher, user, time=10,
+                 app_id="TwitterFollowerBot", source_id="TwitterFollower0"):
+        self.NS = Namespace("http://webtlab.it.uc3m.es/")
+        self.DC = Namespace("http://purl.org/dc/elements/1.1/")
+        self.publisher = publisher
+        self.app_id = app_id
+        self.source_id = source_id
+        self.io_loop = tornado.ioloop.IOLoop.instance()
+        self.num_events_created = 0
+        self.scheduler = utils.get_scheduler("const[" + str(time) + "]")
+        self.client = TwitterRESTclient(user)
+        self.last_id = 0
 
     def toN3(self, tweet):
+        graph = Graph()
+        graph.bind("webtlab", "http://webtlab.it.uc3m.es/")
+        graph.bind("dc", "http://purl.org/dc/elements/1.1/")
+        # Set the triple ID as the tweet ID
+        tweet_id = URIRef("_" + str(tweet.id))
+        # Add the creation timestamp
+        graph.add((tweet_id, self.DC["created"], Literal(tweet.created_at)))
+        # Get the text of the tweet
+        graph.add((tweet_id, self.NS["content"], Literal(tweet.text)))
+        return graph
 
-	graph = Graph()
-
-	graph.bind("webtlab", "http://webtlab.it.uc3m.es/")
-	graph.bind("dc", "http://purl.org/dc/elements/1.1/")
-
-	# Set the triple ID as the tweet ID
-	tweet_id = URIRef("_" + str(tweet.id))
-
-	# Add the creation timestamp
-	graph.add( ( tweet_id, self.DC["created"], Literal(tweet.created_at) ))
-
-	# Get the text of the tweet
-	graph.add( ( tweet_id, self.NS["content"], Literal(tweet.text) ))
-
-	return graph
-
-	
     def schedule_next_event(self):
         self.io_loop.add_timeout(self.scheduler.next(), self.publish_event)
 
+    def publish_event(self):
+        self.schedule_next_event()
+        tweets = self.client.get_tweets(self.last_id)
+        if len(tweets) > 0:
+          print "Generando ",len(tweets)," eventos..."
+          for tweet in tweets:
+            n3rdf = self.toN3(tweet)
+            event = rdfevents.RDFEvent(self.source_id, 'text/n3', n3rdf)
+            self.publisher.publish(event)
+            self.last_id = tweet.id
+        else:
+          print "No hay nuevos eventos que generar"
 
-    def publish_event(self):        
-	
-	self.schedule_next_event()
-	tweets = self.client.get_tweets(self.last_id)
-	if len(tweets) > 0:
-	  print "Generando ",len(tweets)," eventos..."
-	  for tweet in tweets:
-	    n3rdf = self.toN3(tweet)
-	    event = rdfevents.RDFEvent(self.source_id, 'text/n3', n3rdf)
-	    self.publisher.publish(event)  
-	    self.last_id = tweet.id			
-	else:
-	  print "No hay nuevos eventos que generar"	
-        
-
-    def start(self):    	
-       	self.schedule_next_event()
-	self.io_loop.start()
-
+    def start(self):
+        self.schedule_next_event()
+        self.io_loop.start()
 
     def finish(self):
         self.publisher.close()
@@ -83,7 +68,6 @@ class TwitterFollowerBot():
 
 
 def main():
-
     parser = OptionParser()
     parser.add_option("-t", "--time", dest="period",
                   help="Period between two sucessive queries to Twitter API")
@@ -95,13 +79,13 @@ def main():
                   help="Source identifier (to be used in generated events")
     (options, args) = parser.parse_args()
 
-    publisher = client.EventPublisher("http://localhost:9001/events/publish")    
-    bot = TwitterFollowerBot(publisher, options.user, options.period, options.appid, options.source)
-
+    publisher = client.EventPublisher("http://localhost:9001/events/publish") 
+    bot = TwitterFollowerBot(publisher, options.user, options.period,
+                             options.appid, options.source)
     try:
         bot.start()
     except KeyboardInterrupt:
-	bot.finish()
+        bot.finish()
         pass
     finally:
         logger.logger.close()
--- streamsem/examples/twitterSensor.py
+++ streamsem/examples/twitterSensor.py
@@ -1,91 +1,93 @@
 import pycurl
 import cjson as json
-import rdflib
 import tornado
-import traceback
 
 # from geopy import geocoders
 from rdflib import Graph
 from rdflib import Namespace
 from rdflib import Literal
 from rdflib import URIRef
-from tornado.httpclient import AsyncHTTPClient, HTTPRequest, HTTPResponse
+from tornado.httpclient import HTTPRequest, HTTPResponse
 from tornado.simple_httpclient import SimpleAsyncHTTPClient
 
-import streamsem
-from streamsem import events
 from streamsem import rdfevents
 from streamsem import client
 
 class TwitterStreamSensor():
-   
-    def __init__(self, publisher, app_id = "TwitterSensor", source_id = "TwitterSensor0"):
-    	self.STREAM_URL = "https://stream.twitter.com/1/statuses/sample.json"
-	self.USER = "gimi_uc3m"
-	self.PASS = "qabasabslc10"
-	self.NS = Namespace("http://webtlab.it.uc3m.es/")
-	self.DC = Namespace("http://purl.org/dc/elements/1.1/")
-	self.publisher = publisher
-	self.app_id = app_id
-	self.source_id = source_id
-	# self.geo = geocoders.GeoNames()
+
+    def __init__(self, publisher, app_id = "TwitterSensor",
+                 source_id = "TwitterSensor0"):
+        self.STREAM_URL = "https://stream.twitter.com/1/statuses/sample.json"
+        self.USER = "gimi_uc3m"
+        self.PASS = "qabasabslc10"
+        self.NS = Namespace("http://webtlab.it.uc3m.es/")
+        self.DC = Namespace("http://purl.org/dc/elements/1.1/")
+        self.publisher = publisher
+        self.app_id = app_id
+        self.source_id = source_id
+        # self.geo = geocoders.GeoNames()
 
     def toN3(self, tweet_dict):
+        graph = Graph()
 
-	graph = Graph()
+        graph.bind("webtlab", "http://webtlab.it.uc3m.es/")
+        graph.bind("dc", "http://purl.org/dc/elements/1.1/")
 
-	graph.bind("webtlab", "http://webtlab.it.uc3m.es/")
-	graph.bind("dc", "http://purl.org/dc/elements/1.1/")
+        # Set the triple ID as the tweet ID
+        tweet_id = URIRef("_" + str(tweet_dict["id"]))
 
-	# Set the triple ID as the tweet ID
-	tweet_id = URIRef("_" + str(tweet_dict["id"]))
+        # Add the creation timestamp
+        graph.add((tweet_id, self.DC["created"],
+                   Literal(tweet_dict["created_at"])))
 
-	# Add the creation timestamp
-	graph.add( ( tweet_id, self.DC["created"], Literal(tweet_dict["created_at"]) ))
+        # Get the text of the tweet
+        if "text" in tweet_dict:
+            graph.add((tweet_id, self.NS["content"],
+                       Literal(tweet_dict["text"])))
 
-	# Get the text of the tweet
-	if "text" in tweet_dict:
-		graph.add( ( tweet_id, self.NS["content"], Literal(tweet_dict["text"]) ))
+        # Look for entities (urls, mentions, hashtags)
+        if "entities" in tweet_dict:
+            entities = tweet_dict["entities"]
+            if "urls" in entities:
+                for url in entities["urls"]:
+                    graph.add((tweet_id, self.NS["url"],
+                               Literal(url["expanded_url"])))
+            if "hashtags" in entities:
+                for hashtag in entities["hashtags"]:
+                    graph.add((tweet_id, self.NS["hashtag"],
+                               Literal("#" + hashtag["text"])))
+            if "user_mentions" in entities:
+                for mention in entities["user_mentions"]:
+                    graph.add((tweet_id, self.NS["mention"],
+                               Literal("@" + mention["screen_name"])))
 
-	# Look for entities (urls, mentions, hashtags)
-	if "entities" in tweet_dict:
-		entities = tweet_dict["entities"]		
-		if "urls" in entities:
-			for url in entities["urls"]:			
-				graph.add( ( tweet_id, self.NS["url"], Literal(url["expanded_url"]) ))
-		if "hashtags" in entities:
-			for hashtag in entities["hashtags"]:
-				graph.add( ( tweet_id, self.NS["hashtag"], Literal("#" + hashtag["text"]) ))
-		if "user_mentions" in entities:
-			for mention in entities["user_mentions"]:
-				graph.add( ( tweet_id, self.NS["mention"], Literal("@" + mention["screen_name"]) ))
-	
-	# Look for the number of retweets
-	if "retweet_count" in tweet_dict:
-		if tweet_dict["retweet_count"] > 0:
-			graph.add( ( tweet_id, self.NS["retweets"], Literal(str(tweet_dict["retweet_count"])) ))
+        # Look for the number of retweets
+        if "retweet_count" in tweet_dict:
+            if tweet_dict["retweet_count"] > 0:
+                graph.add((tweet_id, self.NS["retweets"],
+                           Literal(str(tweet_dict["retweet_count"]))))
 
-	# Look for geographic information
-	if "coordinates" in tweet_dict:
-		if str(tweet_dict["coordinates"]) != "None":
-			(longitude,latitude) = tweet_dict["coordinates"]["coordinates"]
-			coordinates = "(%s,%s)" % (longitude,latitude)
-			graph.add( ( tweet_id, self.NS["position"], Literal(coordinates) ) )
-
-	return graph
+        # Look for geographic information
+        if "coordinates" in tweet_dict:
+            if str(tweet_dict["coordinates"]) != "None":
+                longitude,latitude = tweet_dict["coordinates"]["coordinates"]
+                coordinates = "(%s,%s)"%(longitude,latitude)
+                graph.add((tweet_id, self.NS["position"],
+                           Literal(coordinates)))
+        return graph
 
     def decode(self, tweet):
-	tweet_dict = json.decode(tweet)
-	# Forget tweets that do not contain id or timestamp
-	if "id" in tweet_dict and "created_at" in tweet_dict:
-	   return self.toN3(tweet_dict)
+        tweet_dict = json.decode(tweet)
+        # Forget tweets that do not contain id or timestamp
+        if "id" in tweet_dict and "created_at" in tweet_dict:
+            return self.toN3(tweet_dict)
 
     def start (self):
-	conn = pycurl.Curl()
-	conn.setopt(pycurl.USERPWD, "%s:%s" % (self.USER, self.PASS))
-	conn.setopt(pycurl.URL, self.STREAM_URL)
-	conn.setopt(pycurl.WRITEFUNCTION, self.on_receive)
-	conn.perform()
+        conn = pycurl.Curl()
+        conn.setopt(pycurl.USERPWD, "%s:%s" % (self.USER, self.PASS))
+        conn.setopt(pycurl.URL, self.STREAM_URL)
+        conn.setopt(pycurl.WRITEFUNCTION, self.on_receive)
+        conn.perform()
 
     def start_async(self):
         http_client = SimpleAsyncHTTPClient()
@@ -100,18 +102,18 @@ class TwitterStreamSensor():
     def on_receive(self, data):
         if isinstance(data, HTTPResponse):
             data = data.body
-	try:
-		graph = self.decode(data)
-		self.publish(graph)	
-	except:
-            # Forget the tweets that produce processing errors	
+        try:
+                graph = self.decode(data)
+                self.publish(graph)
+        except:
+            # Forget the tweets that produce processing errors
             #traceback.print_exc()
             return
 
-    def publish(self, graph):	
-	event = rdfevents.RDFEvent(self.source_id, 'text/n3', graph)
-	print event
-	self.publisher.publish(event)
+    def publish(self, graph):
+        event = rdfevents.RDFEvent(self.source_id, 'text/n3', graph)
+        print event
+        self.publisher.publish(event)
 
 def main():
     publisher = client.EventPublisher("http://localhost:9001/events/publish")
@@ -121,5 +123,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
