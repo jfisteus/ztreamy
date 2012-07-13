fc3053cc3fbd382fc082ec56fbcd193b012fed8a
Jesus Arias Fisteus <jaf@it.uc3m.es>
2012-07-13 12:17:53 +0200

    Fixes in the licensing text.

--- streamsem/__init__.py
+++ streamsem/__init__.py
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see
-# <http://www.gnu.org/
+# <http://www.gnu.org/licenses/>.
 #
 """ A framework for publishing semantic events on the Web."""
 
