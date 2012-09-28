jfisteus/ztreamy

commit b4a246233b0286bf6c9307d224117205eb556a6c

Logging total data received at the client.

ztreamy/client.py
@@ -628,6 +628,7 @@ def main():
         pass
     finally:
         logger.logger.close()
+        logging.info('Bytes received: %d'%transferred_bytes
 
 if __name__ == "__main__":
     main()

commit 45ab41d94b193956d86bee0a8e2d228be24d237f

Fixed a small syntax error introduced in commit b4a246233.

ztreamy/client.py
@@ -628,7 +628,7 @@ def main():
         pass
     finally:
         logger.logger.close()
-        logging.info('Bytes received: %d'%transferred_bytes
+        logging.info('Bytes received: %d'%transferred_bytes)
 
 if __name__ == "__main__":
     main()