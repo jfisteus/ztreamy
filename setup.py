a88bdaaeef299fd9cf95158d34fd3cba04466d24
Jesus Arias Fisteus <jfisteus@gmail.com>
2014-09-18 17:51:57 +0200

    Update version number and changelog.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ if sys.version_info[1] == 6:
 
 setup(
     name = "ztreamy",
-    version = "0.1.5",
+    version = "0.2",
     author = "Jesus Arias Fisteus",
     description = ("A framework for publishing semantic events on the Web"),
     keywords = "rdf sensors web semantic-sensor-web",
