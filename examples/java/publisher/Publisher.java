f11c860988bef6f82cd6fe6eed37be7f0efd3e55
Jesus Arias Fisteus <jfisteus@gmail.com>
2014-09-24 13:05:01 +0200

    New version of Publisher.java that can log data to a file.
    The log file can be replayed by ztreamy.tools.replay

--- examples/java/publisher/Publisher.java
+++ examples/java/publisher/Publisher.java
@@ -2,13 +2,20 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.FileOutputStream;
 
 public class Publisher {
 
     private URL serverURL;
+    private String logFileName;
 
     public Publisher(URL serverURL) {
+        this(serverURL, null);
+    }
+
+    public Publisher(URL serverURL, String logFileName) {
         this.serverURL = serverURL;
+        this.logFileName = logFileName;
     }
 
     public int publish(Event[] events) throws IOException {
@@ -17,10 +24,21 @@ public class Publisher {
         con.setRequestProperty("Content-Type", "application/ztreamy-event");
         con.setDoOutput(true);
         OutputStream out = con.getOutputStream();
+        OutputStream log = null;
+        if (logFileName != null) {
+            log = new FileOutputStream(logFileName, true);
+        }
         for (Event event: events) {
-            out.write(event.serialize());
+            byte[] data = event.serialize();
+            out.write(data);
+            if (logFileName != null) {
+                log.write(data);
+            }
         }
         out.close();
+        if (logFileName != null) {
+            log.close();
+        }
         return con.getResponseCode();
     }
 
@@ -30,12 +48,17 @@ public class Publisher {
 
     public static void main(String[] args)
         throws IOException, InterruptedException {
-        if (args.length != 2) {
+        if (args.length < 2 || args.length > 3) {
             System.err.println("Error: one command-line parameter expected");
-            System.err.println("java Publisher <stream publication URI> <number of events>");
+            System.err.println("java Publisher <stream publication URI> <number of events> [data log file]");
             System.exit(1);
         }
-        Publisher publisher = new Publisher(new URL(args[0]));
+        Publisher publisher;
+        if (args.length == 2) {
+            publisher = new Publisher(new URL(args[0]));
+        } else {
+            publisher = new Publisher(new URL(args[0]), args[2]);
+        }
         int numEvents = Integer.parseInt(args[1]);
         String sourceId = Event.createUUID();
         for (int i = 0; i < numEvents; i++) {
