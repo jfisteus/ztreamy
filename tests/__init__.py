ca342f6d81c3772c5a6e2808e9282ac2108a20b0
Jesus Arias Fisteus <jfisteus@gmail.com>
2015-12-01 18:46:42 +0100

    Add ZlibDispatcher unit tests and fix test discovery

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,22 +1,11 @@
 import unittest
+import os.path
 
+def load_tests(loader, standard_tests, pattern):
+    this_dir = os.path.dirname(__file__)
+    package_tests = loader.discover(start_dir=this_dir, pattern=pattern)
+    standard_tests.addTests(package_tests)
+    return standard_tests
 
 def get_tests():
-    return full_suite()
-
-
-def full_suite():
-    from .test_events import TestEvent
-    from .test_server import TestServer
-    from .test_filters import TestEventTypeFilter
-    from .test_dispatchers import TestSubscriptionGroup
-
-    events_suite = unittest.TestLoader().loadTestsFromTestCase(TestEvent)
-    server_suite = unittest.TestLoader().loadTestsFromTestCase(TestServer)
-    filters_suite = unittest.TestLoader()\
-        .loadTestsFromTestCase(TestEventTypeFilter)
-    dispatchers_suite = unittest.TestLoader()\
-        .loadTestsFromTestCase(TestSubscriptionGroup)
-
-    return unittest.TestSuite([events_suite, server_suite, filters_suite,
-                               dispatchers_suite])
+    return load_tests(unittest.TestLoader(), unittest.TestSuite(), 'test*.py')
--- tests/test_dispatchers.py
+++ tests/test_dispatchers.py
@@ -18,6 +18,8 @@
 
 import unittest
 import zlib
+import random
+import string
 
 import ztreamy.dispatchers
 from ztreamy.dispatchers import (SubscriptionGroup, ClientPropertiesFactory,
@@ -70,6 +72,138 @@ class TestSubscriptionGroup(unittest.TestCase):
                           + events2.serialize(serialization)))
 
 
+class TestZlibDispatcher(unittest.TestCase):
+
+    def test_subscribe(self):
+        rnd = random.Random(45387)
+        uncompressed_data = [
+            _random_string(rnd, 1000),  #0
+            _random_string(rnd, 6000),  #1  c2-3  c-4   c5-7
+            _random_string(rnd, 7000),  #2   7000
+            _random_string(rnd, 4000),  #3  11000  4000
+            _random_string(rnd, 9000),  #4  20000 13000
+            _random_string(rnd, 9000),  #5  29000 22000
+            _random_string(rnd, 4000),  #6  33000 26000  4000
+            _random_string(rnd, 10000), #7        36000 14000
+            _random_string(rnd, 5000),  #8              19000
+            _random_string(rnd, 4000),  #9              23000
+        ]
+        stream = _MockStream('test/stream/path')
+        props = ClientPropertiesFactory.create( \
+                                streaming=True,
+                                encoding=ClientProperties.ENCODING_ZLIB)
+        d = ztreamy.dispatchers.ZlibDispatcher(stream, props)
+        clients = [_MockClient(properties=props) for _ in range(8)]
+        d.dispatch(_MockEventsPack(uncompressed_data[0]))
+        self.assertEqual(len(d.groups), 1)
+        d.subscribe(clients[0])
+        d.subscribe(clients[1])
+        d.dispatch(_MockEventsPack(uncompressed_data[1]))
+        self.assertEqual(len(d.groups), 1)
+        d.subscribe(clients[2])
+        d.subscribe(clients[3])
+        d.dispatch(_MockEventsPack(uncompressed_data[2]))
+        self.assertEqual(len(d.groups), 2)
+        d.subscribe(clients[4])
+        d.dispatch(_MockEventsPack(uncompressed_data[3]))
+        self.assertEqual(len(d.groups), 3)
+        d.dispatch(_MockEventsPack(uncompressed_data[4]))
+        self.assertEqual(len(d.groups), 3)
+        d.dispatch(_MockEventsPack(uncompressed_data[5]))
+        d.subscribe(clients[5])
+        d.subscribe(clients[6])
+        d.subscribe(clients[7])
+        d.dispatch(_MockEventsPack(uncompressed_data[6]))
+        self.assertEqual(len(d.groups), 4)
+        d.dispatch(_MockEventsPack(uncompressed_data[7]))
+        self.assertEqual(len(d.groups), 3) # merge c0-3
+        d.dispatch(_MockEventsPack(uncompressed_data[8]))
+        self.assertEqual(len(d.groups), 2) # merge c0-4
+        d.dispatch(_MockEventsPack(uncompressed_data[9]))
+        self.assertEqual(len(d.groups), 2)
+        data_c0_1 = ''.join(uncompressed_data[1:])
+        self.assertEqual(clients[0].unzlib_sent_data(), data_c0_1)
+        self.assertEqual(clients[1].unzlib_sent_data(), data_c0_1)
+        data_c2_3 = ''.join(uncompressed_data[2:])
+        self.assertEqual(clients[2].unzlib_sent_data(), data_c2_3)
+        self.assertEqual(clients[3].unzlib_sent_data(), data_c2_3)
+        data_c4 = ''.join(uncompressed_data[3:])
+        self.assertEqual(clients[4].unzlib_sent_data(), data_c4)
+        data_c5_7 = ''.join(uncompressed_data[6:])
+        self.assertEqual(clients[5].unzlib_sent_data(), data_c5_7)
+        self.assertEqual(clients[6].unzlib_sent_data(), data_c5_7)
+        self.assertEqual(clients[7].unzlib_sent_data(), data_c5_7)
+
+    def test_unsubscribe(self):
+        rnd = random.Random(45387)
+        uncompressed_data = [
+            _random_string(rnd, 1000),  #0
+            _random_string(rnd, 6000),  #1  c2-3  c-4   c5-7
+            _random_string(rnd, 7000),  #2   7000
+            _random_string(rnd, 4000),  #3  11000  4000
+            _random_string(rnd, 9000),  #4  20000 13000
+            _random_string(rnd, 9000),  #5  29000 22000
+            _random_string(rnd, 4000),  #6  33000 26000  4000
+            _random_string(rnd, 10000), #7        36000 14000
+            _random_string(rnd, 5000),  #8              19000
+            _random_string(rnd, 4000),  #9              23000
+        ]
+        stream = _MockStream('test/stream/path')
+        props = ClientPropertiesFactory.create( \
+                                streaming=True,
+                                encoding=ClientProperties.ENCODING_ZLIB)
+        d = ztreamy.dispatchers.ZlibDispatcher(stream, props)
+        clients = [_MockClient(properties=props) for _ in range(8)]
+        d.dispatch(_MockEventsPack(uncompressed_data[0]))
+        self.assertEqual(len(d.groups), 1)
+        d.subscribe(clients[0])
+        d.subscribe(clients[1])
+        d.dispatch(_MockEventsPack(uncompressed_data[1]))
+        self.assertEqual(len(d.groups), 1)
+        d.subscribe(clients[2])
+        d.subscribe(clients[3])
+        d.unsubscribe(clients[1])
+        d.dispatch(_MockEventsPack(uncompressed_data[2]))
+        self.assertEqual(len(d.groups), 2)
+        d.subscribe(clients[4])
+        d.dispatch(_MockEventsPack(uncompressed_data[3]))
+        self.assertEqual(len(d.groups), 3)
+        d.unsubscribe(clients[2])
+        d.unsubscribe(clients[3])
+        d.dispatch(_MockEventsPack(uncompressed_data[4]))
+        self.assertEqual(len(d.groups), 2)
+        d.dispatch(_MockEventsPack(uncompressed_data[5]))
+        d.subscribe(clients[5])
+        d.subscribe(clients[6])
+        d.subscribe(clients[7])
+        d.dispatch(_MockEventsPack(uncompressed_data[6]))
+        self.assertEqual(len(d.groups), 3)
+        d.dispatch(_MockEventsPack(uncompressed_data[7]))
+        self.assertEqual(len(d.groups), 3) # merge c0-3
+        d.dispatch(_MockEventsPack(uncompressed_data[8]))
+        self.assertEqual(len(d.groups), 2) # merge c0-4
+        d.dispatch(_MockEventsPack(uncompressed_data[9]))
+        self.assertEqual(len(d.groups), 2)
+        data_c0 = ''.join(uncompressed_data[1:])
+        self.assertEqual(clients[0].unzlib_sent_data(), data_c0)
+        data_c1 = ''.join(uncompressed_data[1:2])
+        self.assertEqual(clients[1].unzlib_sent_data(), data_c1)
+        data_c2_3 = ''.join(uncompressed_data[2:4])
+        self.assertEqual(clients[2].unzlib_sent_data(), data_c2_3)
+        self.assertEqual(clients[3].unzlib_sent_data(), data_c2_3)
+        data_c4 = ''.join(uncompressed_data[3:])
+        self.assertEqual(clients[4].unzlib_sent_data(), data_c4)
+        data_c5_7 = ''.join(uncompressed_data[6:])
+        self.assertEqual(clients[5].unzlib_sent_data(), data_c5_7)
+        self.assertEqual(clients[6].unzlib_sent_data(), data_c5_7)
+        self.assertEqual(clients[7].unzlib_sent_data(), data_c5_7)
+
+
+class _MockStream(object):
+    def __init__(self, path):
+        self.path = path
+
+
 class _MockEventsPack(ztreamy.dispatchers.EventsPack):
     def __init__(self, events):
         super(_MockEventsPack, self).__init__(events)
@@ -79,14 +213,19 @@ class _MockEventsPack(ztreamy.dispatchers.EventsPack):
 
 
 class _MockClient(object):
-    def __init__(self):
+    def __init__(self, properties=None):
         self.is_fresh = True
+        self.properties = properties
         self.sent_data = ''
 
     def send(self, data, flush=True):
         self.sent_data += data
         if data:
             self.is_fresh = False
+
+    def unzlib_sent_data(self):
+        return _zlib_decompress_part(self.sent_data)
+
 
 def _zlib_decompress_part(data):
     decompressor = zlib.decompressobj()
@@ -96,3 +235,8 @@ def _zlib_decompress_part(data):
     if decompressor.unused_data:
         raise ValueError('Unused data in zlib decompression')
     return decompressed
+
+_rnd_chars = (string.ascii_lowercase
+              + 12 * ' ' + 5 * 'a' + 6 * 'e' + 2 * 'i' + 3 * 'o' + 2 * 'u')
+def _random_string(random_obj, length):
+    return ''.join(random_obj.choice(_rnd_chars) for _ in range(length))
