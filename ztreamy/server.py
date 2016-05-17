ed20958f8d1b3b2c162ca41548e8fb1f095b0b70
Jesus Arias Fisteus <jfisteus@gmail.com>
2016-05-17 16:55:08 +0200

    Allow passing parameters to the constructor of the Server

--- ztreamy/server.py
+++ ztreamy/server.py
@@ -76,7 +76,8 @@ class StreamServer(tornado.web.Application):
     server.start()
 
     """
-    def __init__(self, port, ioloop=None, stop_when_source_finishes=False):
+    def __init__(self, port, ioloop=None, stop_when_source_finishes=False,
+                 **kwargs):
         """Creates a new server.
 
         'port' specifies the port number in which the HTTP server will
@@ -92,8 +93,7 @@ class StreamServer(tornado.web.Application):
         has been started.
 
         """
-        settings = dict()
-        super(StreamServer, self).__init__(**settings)
+        super(StreamServer, self).__init__(**kwargs)
         logging.info('Initializing server...')
         self.http_server = tornado.httpserver.HTTPServer(self,
                                                 decompress_request=True)
