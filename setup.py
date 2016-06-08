0a460de5bac009a7474f5ea36b83d8971c9f9ca8
Jesus Arias Fisteus <jfisteus@gmail.com>
2016-06-08 13:47:54 +0200

    Update version number

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ tests_require = []
 
 setup(
     name = "ztreamy",
-    version = "0.4.2.dev9",
+    version = "0.4.2.dev10",
     author = "Jesus Arias Fisteus",
     author_email = "jfisteus@gmail.com",
     description = ("A framework for publishing semantic events on the Web"),
