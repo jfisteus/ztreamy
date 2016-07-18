67a556ceba5fd972d0f8ee7365f553411bf3bb6f
Jesus Arias Fisteus <jfisteus@gmail.com>
2016-07-18 17:55:47 +0200

    Update the version of setuptools as required by html5lib

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ if sys.version_info[0] != 2 or sys.version_info[1] not in [7]:
     sys.exit(1)
 
 # Dependencies
-requirements = ['setuptools',
+requirements = ['setuptools>=18.5',
                 'tornado',
                 'rdflib>=4.0',
                 'rdflib-jsonld',
